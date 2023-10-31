c3e70140191ba7151f24c9ea95216f5b68612851
tombisho <trpb2@cantab.net>
2023-10-31 14:35:54 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MRC Epidemiology SRCP'
+copyright = '2023, University of Cambridge'
+author = 'Tom Bishop'
 
 release = '0.1'
 version = '0.1.0'
