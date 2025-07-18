eaefc3e9d325f63b81317c05e35a7d19cf1f5d19
Tom Bishop <trpb2@cantab.net>
2025-07-18 21:24:24 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Epidemiology Unit SRCP'
 copyright = '2023, University of Cambridge'
 author = 'Tom Bishop'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
